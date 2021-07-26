f08c1d605de137a005618cd4d939338bb2fe3ca2
tpqseven <tpqseven@gmail.com>
2021-07-26 17:33:07 +0700

    suafile

--- src/remote/cuacho/TestDriverCuaCho.java
+++ src/remote/cuacho/TestDriverCuaCho.java
@@ -7,13 +7,14 @@ public class TestDriverCuaCho {
         BarkRecognizer barkRecognizer = new BarkRecognizer(cua);
         System.out.println("Fido muon ra ngoai nen sua");
         barkRecognizer.recognizer("gau gau");
-        System.out.println("Fido di ra ngoai");
+         System.out.println("Fido di ra ngoai");
         System.out.println("Fido di tac nghiep");
         System.out.println("Cua cho dong");
         System.out.println("Fido di tac nghiep xong, fido muon vo nha ne sua");
         barkRecognizer.recognizer("au au");
         System.out.println("Fido di vao nha");
         System.out.println("Cua cho dong");
+        //check check tao moi sua
         
     }
 }
